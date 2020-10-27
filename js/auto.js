3fbaf588479a567c90f078cc38b183e5dbdad55c
david-kishi <dansdk.blue@gmail.com>
2020-10-26 22:01:34 -0700

    Refactored telemetry dictionary into a class with update and status methods.

--- js/auto.js
+++ js/auto.js
@@ -4,8 +4,9 @@
  * This javascript file serves the purpose of automating the docking sequences
  * of SpaceX Dragon 2 capsule to the ISS.
  * 
- * Dragon Functions
- * --
+ * ----------------------------------------------------------------------------
+ * Dragon Functions (from threed.min.js)
+ * ----------------------------------------------------------------------------
  * rollLeft(), rollRight()
  * pitchDown(), pitchUp()
  * yawLeft(), yawRight()
@@ -13,92 +14,87 @@
  * translateDown(), translateUp(), translateRight(), translateLeft()
  * toggleTranslation(), toggleRotation()
  * 
+ * ----------------------------------------------------------------------------
+ * Dragon Telemetry (from threed.min.js)
+ * ----------------------------------------------------------------------------
+ * pitchError: fixedRotationX
+ * yawError: fixedRotationY
+ * rollError: fixedRotationZ
+ * 
+ * pitchRate: t.toFixed(1)
+ * yawRate: e.toFixed(1)
+ * rollRate: o.toFixed(1)
+ * 
+ * x-distance: a.toFixed(1)
+ * y-distance: r.toFixed(1)
+ * z-distance: i.toFixed(1)
+ * 
+ * range: n.toFixed(1)
+ * range-rate: smoothRangeRate.toFixed(3)
  */
 
+// Initiate auto sequence
 document.getElementById("auto-button").addEventListener("click", function () {
   hideIntro();
-  setTimeout(correctionInterval, 10000);
 });
 
-// Initiate Dragon's Object
-// TODO Convert to class
-var dragon = {
-  x: 0.0,
-  y: 0.0,
-  z: 0.0,
-  xRate: 0,
-  yRate: 0,
-  zRate: 0,
-  rate: 0.0,
-  rollError: 0.0,
-  rollRate: 0.0,
-  pitchError: 0.0,
-  pitchRate: 0.0,
-  yawError: 0.0,
-  yawRate: 0.0,
-};
+// Dragon Telemetry Class
+class DragonTelemetry {
 
-function telemetryUpdate() {
-  dragon.x = parseFloat(document.getElementById("x-range").innerText);
-  dragon.y = parseFloat(document.getElementById("y-range").innerText);
-  dragon.z = parseFloat(document.getElementById("z-range").innerText);
-  dragon.rate = Math.abs(parseFloat(document.getElementById("rate").children[1].innerText));
-  dragon.rollError = parseFloat(document.getElementById("roll").children[0].innerText);
-  dragon.rollRate = parseFloat(document.getElementById("roll").children[1].innerText);
-  dragon.pitchError = parseFloat(document.getElementById("pitch").children[0].innerText);
-  dragon.pitchRate = parseFloat(document.getElementById("pitch").children[1].innerText);
-  dragon.yawError = parseFloat(document.getElementById("yaw").children[0].innerText);
-  dragon.yawRate = parseFloat(document.getElementById("yaw").children[1].innerText);
+  // Initialize telemetry (this may initialize to 0 if called within the first ~5 seconds)
+  constructor() {
+    this.xDistance = camera.position.z - issObject.position.z;
+    this.yDistance = camera.position.x - issObject.position.x;
+    this.zDistance = camera.position.y - issObject.position.y;
+    this.pitchError = fixedRotationX;
+    this.yawError = fixedRotationY;
+    this.rollError = fixedRotationZ;
+    this.pitchRate = .1 * rateRotationX;
+    this.yawRate = .1 * rateRotationY;
+    this.rollRate = .1 * rateRotationZ;
+    this.range = Math.sqrt(this.xDistance * this.xDistance +
+      this.yDistance * this.yDistance +
+      this.zDistance * this.zDistance);
+    this.rangeRate = smoothRangeRate.toFixed(3);
+  }
+
+  // Updates telemetry
+  update() {
+    this.xDistance = (camera.position.z - issObject.position.z).toFixed(1);
+    this.yDistance = (camera.position.x - issObject.position.x).toFixed(1);
+    this.zDistance = (camera.position.y - issObject.position.y).toFixed(1);
+    this.pitchError = fixedRotationX;
+    this.yawError = fixedRotationY;
+    this.rollError = fixedRotationZ;
+    this.pitchRate = .1 * rateRotationX;
+    this.yawRate = .1 * rateRotationY;
+    this.rollRate = .1 * rateRotationZ;
+    this.range = Math.sqrt(this.xDistance * this.xDistance +
+      this.yDistance * this.yDistance +
+      this.zDistance * this.zDistance);
+    this.rangeRate = smoothRangeRate.toFixed(3);
+  }
+
+  // Console logs all telemetry
+  status() {
+    console.log("=============== TELEMETRY ===============");
+    console.log("Range: ", this.range);
+    console.log("Rate: ", this.rangeRate);
+    console.log("x: ", this.xDistance);
+    console.log("y: ", this.yDistance);
+    console.log("z: ", this.zDistance);
+    console.log("Pitch Error: ", this.pitchError);
+    console.log("Pitch Rate: ", this.pitchRate);
+    console.log("Yaw Error: ", this.yawError);
+    console.log("Yaw Rate: ", this.yawRate);
+    console.log("Roll Error: ", this.rollError);
+    console.log("Roll Rate: ", this.rollRate);
+  }
 }
 
-/***
- * Correct roll under these conditions.
- * 
- * rollRight:
- *  - rollError (0,inf) & rollRate > 0
- *  - rollError [0,2] & rollRate < 0
- *  - 
- * rollLeft:
- *  - rollError (-inf, 0) & rollRate < 0
- *  - rollError [0,2] & rollRate > 0
- */
-function correctRoll() {
-  // TODO correct dragon's roll
+let dragTel = new DragonTelemetry();
 
-};
-
-function correctPitch() {
-  // TODO correct dragon's pitch
-};
-
-function correctYaw() {
-  // TODO correct dragon's yaw
-}
-
-/***
- * Correcting Axis
- * 
- * x burn scale
- * [200 - - - - - - 100 - - - 50 - 25 - 0]
- * [2               m2        1    m1  m1]
- * 
- * y burn scale
- * [-15 - - -10 - - -5 - - 0 - - 5 - - 10 - - 15]
- * [+2       m2     +1          -1     m-2    -2]
- * 
- * z burn scale
- * [-15 - - -10 - - -5 - - 0 - - 5 - - 10 - - 15]
- * [+2       m2     +1          -1     m-2    -2]
- */
-function correctAxis() {
-
-};
-
-function corrections() {
-  telemetryUpdate();
-  correctAxis();
-};
-
-function correctionInterval() {
-  setInterval(corrections, 500);
-};
+setInterval(function () {
+  dragTel.update();
+  dragTel.status();
+}, 100)
