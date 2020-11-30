496f36df5f14b6c7d7184e851f3f97f269d1c5ec
David Nguyen <dansdk.blue@gmail.com>
2020-11-30 02:41:41 -0800

    Moved dragon telemetry class to it's own exclusive file. Removed it from auto.js. Changed telemetry precision to 3 decimal places.

--- js/auto.js
+++ js/auto.js
@@ -13,77 +13,7 @@
  * translateForward(), translateBackward()
  * translateDown(), translateUp(), translateRight(), translateLeft()
  * toggleTranslation(), toggleRotation()
- * 
- * ----------------------------------------------------------------------------
- * Dragon Telemetry (from threed.min.js)
- * ----------------------------------------------------------------------------
- * pitchError: fixedRotationX
- * yawError: fixedRotationY
- * rollError: fixedRotationZ
- * 
- * pitchRate: t.toFixed(1)
- * yawRate: e.toFixed(1)
- * rollRate: o.toFixed(1)
- * 
- * x-distance: a.toFixed(1)
- * y-distance: r.toFixed(1)
- * z-distance: i.toFixed(1)
- * 
- * range: n.toFixed(1)
- * range-rate: smoothRangeRate.toFixed(3)
  */
-
-// Dragon Telemetry Class
-class DragonTelemetry {
-
-  // Initialize telemetry (this may initialize to 0 if called within the first ~5 seconds)
-  constructor() {
-    this.xDistance = 999.99;
-    this.yDistance = 999.99;
-    this.zDistance = 999.99;
-    this.pitchError = 999.99;
-    this.yawError = 999.99;
-    this.rollError = 999.99;
-    this.pitchRate = 999.99;
-    this.yawRate = 999.99;
-    this.rollRate = 999.99;
-    this.range = 999.99;
-    this.rangeRate = 999.99;
-  }
-
-  // Updates telemetry
-  update() {
-    this.xDistance = (camera.position.z - issObject.position.z);
-    this.yDistance = (camera.position.x - issObject.position.x);
-    this.zDistance = (camera.position.y - issObject.position.y);
-    this.pitchError = parseFloat(fixedRotationX);
-    this.yawError = parseFloat(fixedRotationY);
-    this.rollError = parseFloat(fixedRotationZ);
-    this.pitchRate = .1 * rateRotationX;
-    this.yawRate = .1 * rateRotationY;
-    this.rollRate = .1 * rateRotationZ;
-    this.range = Math.sqrt(this.xDistance * this.xDistance +
-      this.yDistance * this.yDistance +
-      this.zDistance * this.zDistance);
-    this.rangeRate = smoothRangeRate;
-  }
-
-  // Console logs all telemetry
-  status() {
-    console.log("=============== TELEMETRY ===============");
-    console.log("Range: ", this.range);
-    console.log("Rate: ", this.rangeRate);
-    console.log("x: ", this.xDistance);
-    console.log("y: ", this.yDistance);
-    console.log("z: ", this.zDistance);
-    console.log("Pitch Error: ", this.pitchError);
-    console.log("Pitch Rate: ", this.pitchRate);
-    console.log("Yaw Error: ", this.yawError);
-    console.log("Yaw Rate: ", this.yawRate);
-    console.log("Roll Error: ", this.rollError);
-    console.log("Roll Rate: ", this.rollRate);
-  }
-}
 
 // Create Dragon Telemetry object
 var dragTel = new DragonTelemetry();
---
+++ js/dragontelemetry.js
@@ -0,0 +1,60 @@
+/**
+ * dragontelemetry.js
+ * --
+ * This javascript file serves the purpose of declaring the Dragon Telemtry
+ * class. This class will hold all current telemetry and update functions.
+ */
+
+// Dragon Telemetry Class
+class DragonTelemetry {
+
+  // Initialize telemetry (this may initialize to 0 if called within the first ~5 seconds)
+  constructor() {
+    this.xDistance = 999.999;
+    this.yDistance = 999.999;
+    this.zDistance = 999.999;
+    this.pitchError = 999.999;
+    this.yawError = 999.999;
+    this.rollError = 999.999;
+    this.pitchRate = 999.999;
+    this.yawRate = 999.999;
+    this.rollRate = 999.999;
+    this.range = 999.999;
+    this.rangeRate = 999.999;
+    this.yRate = 999.999;
+    this.zRate = 999.999;
+  }
+
+  // Updates telemetry
+  update() {
+    this.xDistance = (camera.position.z - issObject.position.z);
+    this.yDistance = (camera.position.x - issObject.position.x);
+    this.zDistance = (camera.position.y - issObject.position.y);
+    this.pitchError = parseFloat(fixedRotationX);
+    this.yawError = parseFloat(fixedRotationY);
+    this.rollError = parseFloat(fixedRotationZ);
+    this.pitchRate = .1 * rateRotationX;
+    this.yawRate = .1 * rateRotationY;
+    this.rollRate = .1 * rateRotationZ;
+    this.range = Math.sqrt(this.xDistance * this.xDistance +
+      this.yDistance * this.yDistance +
+      this.zDistance * this.zDistance);
+    this.rangeRate = smoothRangeRate;
+  }
+
+  // Console logs all telemetry
+  status() {
+    console.log("=============== TELEMETRY ===============");
+    console.log("Range: ", this.range);
+    console.log("Rate: ", this.rangeRate);
+    console.log("x: ", this.xDistance);
+    console.log("y: ", this.yDistance);
+    console.log("z: ", this.zDistance);
+    console.log("Pitch Error: ", this.pitchError);
+    console.log("Pitch Rate: ", this.pitchRate);
+    console.log("Yaw Error: ", this.yawError);
+    console.log("Yaw Rate: ", this.yawRate);
+    console.log("Roll Error: ", this.rollError);
+    console.log("Roll Rate: ", this.rollRate);
+  }
+};
